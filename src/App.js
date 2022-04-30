536e08169fdc7075982bba4d5487951f4e50cda0
livebrightly <69799297+livebrightly@users.noreply.github.com>
2022-04-29 21:14:07 -0400

    Add student state to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [
+        {
+          name: "Cait Yomorta",
+          bio: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum",
+          scores: [],
+        },
+        {
+          name: "Holly Baird",
+          bio: "Eum molestiae explicabo deserunt, maiores quod eaque omnis tenetur vero ducimus, magnam autem! Quia facere quaerat eum repudiandae dolorum eligendi iure quae. Eos id possimus accusantium, earum animi modi hic.",
+          scores: [],
+        },
+        {
+          name: "Wes Mungia",
+          bio: "Repudiandae veritatis recusandae quidem tenetur impedit, numquam incidunt enim, adipisci id cupiditate asperiores nam perferendis. Facere odit laborum ipsum autem repellendus natus eius doloremque ullam perferendis. Enim repellendus ut veniam?",
+          scores: [],
+        },
+      ],
+    };
+  }
 }
 
 export default App;
